3af74f4143a81d4e536145966cba87a84e5652ef
sfc-gh-jsoliz <javier.soliz@snowflake.com>
2024-01-29 15:43:11 -0700

    added cli args for local vs remote

--- app.py
+++ app.py
@@ -1,19 +1,40 @@
+# on:  (hello-snowpark-git-py39) /Users/jsoliz/src/github/sfc-gh-jsoliz/hello-snowpark-git:hello-snowpark-git $
+# run: python app_connect_to_snow.py --mode remote
+
 from __future__ import annotations
 from snowflake.snowpark.session import Session
 from snowflake.snowpark import DataFrame
 from snowflake.snowpark.functions import col
+import json
+import argparse
 
 
-def hello(session: Session) -> DataFrame:
+
+def hello_local(session: Session) -> DataFrame:
     df = session.table("products")
     return df.filter(col("ID") == 1)
+
+def hello_remote(session: Session) -> DataFrame:
+    df = session.table("git.test_data.customers")
+    #df = df.groupBy("STATE").count()
+    return df
 
 
 # For local debugging
 if __name__ == "__main__":
-    from snowflake.snowpark.mock.mock_connection import MockServerConnection
-    import init_local
+    parser = argparse.ArgumentParser(description="at CLI take local or remote for local testing or connecting to a snow instance")
+    parser.add_argument("--mode", choices=["local", "remote"], help="Choose mode (local or remote)")
+    args = parser.parse_args()
 
-    session = Session(MockServerConnection())  # type: ignore
-    session = init_local.init(session)
-    print(hello(session).show())
+    if args.mode == 'local':
+        from snowflake.snowpark.mock.connection import MockServerConnection
+        import init_local
+
+        session = Session(MockServerConnection())  # type: ignore
+        session = init_local.init(session)
+        print(hello_local(session).show())
+    
+    if args.mode == 'remote':
+        session = Session.builder.configs(json.load(open("/Users/jsoliz/.creds/snowflake_connection.json"))).create()
+        print(hello_remote(session).show())
+
